059e6d62922dd9548bc63bdc2b688f3fc9aac56c
Noah Aviel Dove <nadove@ucsc.edu>
2023-02-13 19:26:12 -0800

    Structure project as package

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='bijection',
+    version='1.0.0',
+    description='A single-file package implementing a mutable, bidirectional, 1-to-1 mapping with O(1) item access.',
+    url='https://github.com/noah-aviel-dove/bijection',
+    author_email='noah.aviel.dove@gmail.com',
+    license='GPLv3',
+    packages=['bijection'],
+    python_requires='>=3.11.1'
+)
